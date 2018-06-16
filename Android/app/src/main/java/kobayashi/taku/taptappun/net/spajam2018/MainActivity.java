f94476eabcbfa1a29b9df2385007666931de5de8
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2018-06-16 15:25:08 +0900

    permission問題に陥らないためにライブラリ追加しておく

--- Android/app/src/main/java/kobayashi/taku/taptappun/net/spajam2018/MainActivity.java
+++ Android/app/src/main/java/kobayashi/taku/taptappun/net/spajam2018/MainActivity.java
@@ -4,12 +4,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import net.taptappun.taku.kobayashi.runtimepermissionchecker.RuntimePermissionChecker;
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private static final int REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        RuntimePermissionChecker.requestAllPermissions(this, REQUEST_CODE);
     }
 
     /**
